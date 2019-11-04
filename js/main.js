12aa6074c475c1ba06ce201538604810cad382c0
k-calderon <bluelotus79@gmail.com>
2019-11-03 18:16:41 -0800

    finish navbar

--- js/main.js
+++ js/main.js
@@ -74,15 +74,28 @@ var log = function(msg, obj, name) {
 const navSlide = () => {
   const burger = d.querySelector(".burger");
   const nav = d.querySelector(".nav-links");
+  const navLinks = d.querySelectorAll(".nav-links li");
+
+  // Toggle nav
   burger.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
-  })
+
+    // Animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+      }
+    });
+    
+    // Burger animation
+    burger.classList.toggle("toggle");
+  });  
 }
 
 function init() {
-  // do stuff
   navSlide();
 }
 
-// boot
 init();
